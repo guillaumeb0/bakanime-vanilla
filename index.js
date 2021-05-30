973e2e9efb534a810409fa4b457231c329521720
guillaumeb0 <guillaume.bakaga@edu.itescia.fr>
2021-05-30 09:31:23 +0200

    broken wip

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const fetchData = async (url, responseKey) => {
     try {
       const res = await axios.get(url)
-      return res.data[responseKey]
+      return res.data[responseKey].map((e, i) => ({id: i, ...e}))
     } catch (error) {
       return null
     }
@@ -16,28 +16,32 @@ document.addEventListener('DOMContentLoaded', async () => {
       {
         name: 'todayReleases',
         animes: await fetchData(`https://api.jikan.moe/v3/schedule/${currentDayName}`, currentDayName.toLowerCase()),
-        displayCursor: 0,
+        currentFirstDisplayedCard: 0,
+        currentLastDisplayedCard: 6,
         htmlContainer: document.querySelector('.today-releases .anime-list-content')
 
       },
       {
         name: 'airing',
         animes: await fetchData('https://api.jikan.moe/v3/top/anime/1/airing', 'top'),
-        displayCursor: 0,
+        currentFirstDisplayedCard: 0,
+        currentLastDisplayedCard: 6,
         htmlContainer: document.querySelector('.airing-animes .anime-list-content')
 
       },
       {
         name: 'topUpcoming',
         animes: await fetchData('https://api.jikan.moe/v3/top/anime/1/upcoming', 'top'),
-        displayCursor: 0,
+        currentFirstDisplayedCard: 0,
+        currentLastDisplayedCard: 6,
         htmlContainer: document.querySelector('.top-upcoming .anime-list-content')
 
       },
       {
         name: 'topManga',
         animes: await fetchData('https://api.jikan.moe/v3/top/manga', 'top'),
-        displayCursor: 0,
+        currentFirstDisplayedCard: 0,
+        currentLastDisplayedCard: 6,
         htmlContainer: document.querySelector('.top-manga .anime-list-content')
 
       },
@@ -54,10 +58,10 @@ document.addEventListener('DOMContentLoaded', async () => {
     document.body.classList.remove('no-scroll')
   })
 
-  const createCard = ({image_url, title, type, start_date}) => {
+  const createCard = ({id, imageUrl, title, type, startDate}) => {
     const cardHtmlString = `
-<div class="card">
-  <img src="${image_url}" alt="${title}_avatar">
+<div class="card" data-id="${id}">
+  <img src="${imageUrl}" alt="${title}_avatar">
   <div class="card-description">
     <h3 class="card-description-title">${title}</h3>
     <div class="play-cta-wrapper">
@@ -65,7 +69,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     </div>
     <div class="card-description-text">
       <div class="type">${type}</div>
-      ${start_date ? `<div class="start-date">${start_date}</div>` : ''}
+      ${startDate ? `<div class="start-date">${startDate}</div>` : ''}
     </div>
   </div>
 </div>
@@ -76,32 +80,25 @@ document.addEventListener('DOMContentLoaded', async () => {
     return template.content.firstElementChild
   }
 
-  const appendAnimeList = (list, data) => {
-    data.forEach((anime) => {
-      let card = createCard({
-        image_url: anime.image_url,
-        title: anime.title,
-        type: anime.type,
-        start_date: anime.start_date
-      })
+  const appendAnimeList = (list, cards) => {
+    Array.from(cards).forEach((card) => {
       list.appendChild(card)
     })
   }
 
-  const prependAnimeList = (list, data) => {
-    data.forEach((anime) => {
-      let card = createCard({
-        image_url: anime.image_url,
-        title: anime.title,
-        type: anime.type,
-        start_date: anime.start_date
-      })
-      list.prepend(card)
-    })
+  const prependAnimeList = (list, cards) => {
+    cards.forEach((card) => list.prepend(card))
   }
 
-  const deleteCards = (list, nodes) => {
-    nodes.forEach(node => list.removeChild(node))
+  const pruneExtraOuterCards = ({startIndex, bucketLength, htmlList}) => {
+    const indexesToPrune = getIndexesToPrune(startIndex, bucketLength)
+    console.log(indexesToPrune)
+    htmlList.childNodes.forEach(node => {
+      if (node.nodeType === 1 && indexesToPrune.includes(parseInt(node.dataset.id))) {
+        console.log('to remove:', node)
+        htmlList.removeChild(node)
+      }
+    })
   }
 
   const markFirstAndLastVisibleCards = (list) => {
@@ -119,26 +116,60 @@ document.addEventListener('DOMContentLoaded', async () => {
     orderedVisibleElements[orderedVisibleElements.length - 1].dataset.visibleScrollPosition = 'last'
   }
 
-  Object.values(state.buckets).forEach(bucket => {
-    appendAnimeList(bucket.htmlContainer, bucket.animes.slice(bucket.displayCursor, state.displayedCardCount))
-    markFirstAndLastVisibleCards(bucket.htmlContainer)
-  })
-
   // I'm terrible at maths ! I have no idea how to name this !
   const foo = (num, length) => num >= 0 ? num : length + num
 
-  const getPreviousScrollInfo = bucket => {
-    const length = bucket.animes.length
-    const nextRangeStart = foo(bucket.displayCursor - state.displayedCardCount, length)
-    const nextRangeEnd = foo(bucket.displayCursor - 1, length)
+  const getPreviousScrollInfo = (currentStartIndex, length) => {
+    const nextRangeStart = foo(currentStartIndex - state.displayedCardCount, length)
+    const nextRangeEnd = foo(currentStartIndex - 1, length)
 
     return {
       rangeStart: nextRangeStart,
+      rangeEnd: nextRangeEnd,
       indexes: Array.from({length: state.displayedCardCount}, (_, i) => {
         return foo(nextRangeEnd - i, length)
-      })
+      }).reverse()
     }
   }
+
+  const getRangeFromIndex = (index, length) => {
+    return Array.from({length: state.displayedCardCount}, (_, i) => (index + i) % length)
+  }
+
+  const animesToCards = (animes, dataIndexes) => {
+    return dataIndexes.map(i => animes[i]).map((anime) => {
+      return createCard({
+        id: anime.id,
+        imageUrl: anime.image_url,
+        title: anime.title,
+        type: anime.type,
+        startDate: anime.start_date
+      })
+    })
+  }
+
+  const getIndexesToPrune = (startIndex, length) => {
+    const previousRange = getPreviousScrollInfo(startIndex, length).indexes
+    const currentRange = getRangeFromIndex(startIndex, length)
+    const nextRange = getRangeFromIndex(startIndex + state.displayedCardCount, length)
+
+    const indexesToKeep = previousRange.concat(currentRange).concat(nextRange)
+    console.log('index to keep:', indexesToKeep)
+    return Array.from({length: length}, (_, i) => i).filter(e => !indexesToKeep.includes(e))
+  }
+
+  Object.values(state.buckets).forEach(bucket => {
+    const previousScrollInfo = getPreviousScrollInfo(bucket.currentFirstDisplayedCard, bucket.animes.length)
+    const currentRange = getRangeFromIndex(bucket.currentFirstDisplayedCard, bucket.animes.length)
+    const nextRange = getRangeFromIndex(bucket.currentFirstDisplayedCard + state.displayedCardCount, bucket.animes.length)
+    const previousCards = animesToCards(bucket.animes, previousScrollInfo.indexes)
+    const currentCards = animesToCards(bucket.animes, currentRange)
+    const nextCards = animesToCards(bucket.animes, nextRange)
+
+    appendAnimeList(bucket.htmlContainer, previousCards.concat(currentCards).concat(nextCards))
+    bucket.htmlContainer.scrollBy({top: 0, left: currentCards[0].offsetWidth * state.displayedCardCount})
+    markFirstAndLastVisibleCards(bucket.htmlContainer)
+  })
 
   const navigateBeforeButtons = document.querySelectorAll('.navigation-icon.navigate-before')
   navigateBeforeButtons.forEach(button => {
@@ -148,12 +179,12 @@ document.addEventListener('DOMContentLoaded', async () => {
       const cardWidth = listContent.querySelector('.card').offsetWidth || 0
 
       const bucket = Object.values(state.buckets).find(value => value.htmlContainer == listContent)
-      const currentCards = Array.from(listContent.childNodes)
-      const previousScrollInfo = getPreviousScrollInfo(bucket)
+      const previousScrollInfo = getPreviousScrollInfo(bucket.currentFirstDisplayedCard, bucket.animes.length)
+      const newCards = animesToCards(bucket.animes, previousScrollInfo.indexes.reverse())
 
-      const newCardsData = previousScrollInfo.indexes.map(i => bucket.animes[i])
-      prependAnimeList(listContent, newCardsData)
-      bucket.displayCursor = previousScrollInfo.rangeStart
+      prependAnimeList(listContent, newCards)
+      bucket.currentFirstDisplayedCard = previousScrollInfo.rangeStart
+      bucket.currentLastDisplayedCard = previousScrollInfo.rangeEnd
 
       listContent.scrollBy({
         top: 0,
@@ -162,22 +193,12 @@ document.addEventListener('DOMContentLoaded', async () => {
       })
 
       setTimeout(() => {
-        deleteCards(listContent, currentCards)
+        pruneExtraOuterCards(listContent)
       }, 1000)
 
       // updateFirstAndLastVisibleCardsOnScrollForward(listContent)
     })
   })
-
-  const getNextScrollInfo = bucket => {
-    const nextRangeStart = bucket.displayCursor + state.displayedCardCount
-    const length = bucket.animes.length
-
-    return {
-      rangeStart: nextRangeStart,
-      indexes: Array.from({length: state.displayedCardCount}, (_, i) => (nextRangeStart + i) % length)
-    }
-  }
 
   const navigateNextButtons = document.querySelectorAll('.navigation-icon.navigate-next')
   navigateNextButtons.forEach(button => {
@@ -185,22 +206,32 @@ document.addEventListener('DOMContentLoaded', async () => {
       const listContent = event.target.closest('.anime-list').querySelector('.anime-list-content')
       const cardWidth = listContent.querySelector('.card').offsetWidth || 0
 
-      const bucket = Object.values(state.buckets).find(value => value.htmlContainer == listContent)
-      const currentCards = Array.from(listContent.childNodes)
-      const nextScrollInfo = getNextScrollInfo(bucket)
-      const newCardsData = nextScrollInfo.indexes.map(i => bucket.animes[i])
-      appendAnimeList(listContent, newCardsData)
-      bucket.displayCursor = nextScrollInfo.rangeStart
-
       listContent.scrollBy({
         top: 0,
         left: cardWidth * state.displayedCardCount,
         behavior: 'smooth'
       })
 
-      setTimeout(() => {
-        deleteCards(listContent, currentCards)
-      }, 1000)
+      // Create and append next cards
+      const bucket = Object.values(state.buckets).find(value => value.htmlContainer == listContent)
+      const nextRange = getRangeFromIndex(
+        bucket.currentFirstDisplayedCard + state.displayedCardCount * 2,
+        bucket.animes.length
+      )
+
+      const newCards = animesToCards(bucket.animes, nextRange)
+      appendAnimeList(listContent, newCards)
+
+      // Prune extra outer cards
+      pruneExtraOuterCards({
+        startIndex: bucket.currentFirstDisplayedCard + state.displayedCardCount,
+        bucketLength: bucket.animes.length,
+        htmlList: listContent
+      })
+
+      // Update bucket info
+      bucket.currentFirstDisplayedCard += state.displayedCardCount
+      bucket.currentLastDisplayedCard += state.displayedCardCount
 
 
       // updateFirstAndLastVisibleCardsOnScrollForward(listContent)
